239d0d09cf961520ced5c1eb1314b4b65067750a
Danny Armijos <71116392+dmarmijosa@users.noreply.github.com>
2022-07-12 22:53:35 +0200

    ejercicio2

---
+++ secccion 5/ejercicio2.py
@@ -0,0 +1,11 @@
+def es_primo(num):
+    for n in range(2, num):
+        if num % n == 0:
+            print("No es primo")
+            return False
+    print("Es primo")
+    return True
+
+v = int(input('ingrese el numero a verificar '))
+
+es_primo(v)
